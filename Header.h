5e8981646b25c7f197bc58d7ccacd8df7e9c4b00
oraclechang <oraclechang@gmail.com>
2012-08-28 21:43:57 +0900

    first commit

---
+++ Header.h
@@ -0,0 +1,25 @@
+// STD lib
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// STL Sequence Container
+#include <vector>
+#include <deque>
+#include <list>
+
+// STL Associative Container
+#include <set>
+#include <map>
+
+// STL Container Adaptor
+#include <stack>
+#include <queue>
+#include <bitset>
+
+// STL Algorithm and iterator
+#include <algorithm>
+#include <memory>
+#include <iterator>
+
+using namespace std;
---
+++ MergeSort.cpp
@@ -0,0 +1,11 @@
+#ifdef MERGESORT
+
+#include "Header.h
+
+void MergeSort(
+int main()
+{
+	return 0;
+}
+
+#endif
