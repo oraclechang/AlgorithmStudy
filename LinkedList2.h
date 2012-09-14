98944ee913f88e1ba5c131317a4dd3a8db66fdb6
oraclechang <oraclechang@gmail.com>
2012-09-14 22:49:57 +0900

    Added LinkedList

---
+++ LinkedList2.h
@@ -0,0 +1,19 @@
+#ifdef _LINKED_LIST_
+
+#include "Header.h"
+
+struct Node
+{
+	Node* pNext;
+	int data;
+
+	Node(int _data) : data(_data) {}
+};
+
+// Insert
+// Delete
+// Search
+// Travelse
+
+
+#endif
---
+++ LinkedListTest.cpp
@@ -0,0 +1,17 @@
+#ifdef _LINKED_LIST_
+
+/*
+Problem
+
+Solution
+
+*/
+
+#include "Header.h"
+#include "LinkedList2.h"
+
+void main()
+{
+}
+
+#endif
