dc3094faea5305473ab66f4ef1cd01515f60c329
oraclechang <oraclechang@gmail.com>
2012-09-16 09:31:53 +0900

    Added LinkedList_shared_ptr.cpp

---
+++ LinkedList_shared_ptr.cpp
@@ -0,0 +1,124 @@
+#ifdef _LINKED_LIST_SMARTPTR_
+
+#include "Header.h"
+
+struct Node
+{
+	shared_ptr< Node > pNext;
+	int data;
+
+	Node(int _data) : data(_data), pNext(nullptr) {}
+};
+
+
+// Insert
+//bool InsertNodeAtAfter(Node** pNode, int data)
+bool InsertNodeAtAfter(shared_ptr< Node >* ppNode, int data)
+{
+	shared_ptr< Node > pNewNode(new Node(data));
+
+	if (nullptr == *ppNode)
+		*ppNode = pNewNode;
+	else
+	{
+		shared_ptr< Node>  pTmp = *ppNode;
+		pNewNode->pNext = pTmp->pNext;
+		pTmp->pNext = pNewNode;
+	}
+
+	return true;
+}
+
+// Delete
+// Search
+// Travelse
+
+//bool InsertNodeAtTail(Node** ppNode, int data)
+bool InsertNodeAtTail(shared_ptr< Node >* ppNode, int data)
+{
+	shared_ptr< Node > pNewNode(new Node(data));
+
+	if (nullptr == *ppNode)
+		*ppNode = pNewNode;
+	else
+	{
+		shared_ptr< Node > pTmp = *ppNode;
+
+		while (nullptr != pTmp->pNext)
+			pTmp = pTmp->pNext;
+
+		pTmp->pNext = pNewNode;
+	}
+
+	return true;
+}
+
+bool DeleteNode(shared_ptr< Node >* ppNode,const int data)
+{
+	if (nullptr == *ppNode)
+		return false;
+
+	shared_ptr< Node > pTmp;
+	pTmp = *ppNode;
+	if (data == (*ppNode)->data)
+	{
+		*ppNode = (*ppNode)->pNext;
+
+		//delete pTmp;
+	}
+	else
+	{
+		shared_ptr< Node > pPrevNode = pTmp;
+		while (nullptr != pTmp)
+		{
+			if (data == pTmp->data)
+			{
+				pPrevNode->pNext = pTmp->pNext;
+				//delete pTmp;
+
+				return true;
+			}
+			pPrevNode = pTmp;
+			pTmp = pTmp->pNext;
+		}
+	}
+
+	return false;
+}
+
+void TravelseNode(shared_ptr< Node > pHead)
+{
+	while (nullptr != pHead)		// 주의!!! pHead->pNext 가 null일때가지 출력하면 마지막 node 가 출력되지 않음
+	{
+		cout << pHead->data << " ";
+		pHead = pHead->pNext;
+	}
+	cout << endl;
+}
+
+void main()
+{
+	shared_ptr< Node > pHead(nullptr);
+
+	/*
+	InsertAfter(&pHead, 1);
+	InsertAfter(&pHead, 2);
+	InsertAfter(&pHead, 3);
+	InsertAfter(&pHead, 4);
+	*/
+	InsertNodeAtTail(&pHead, 1);
+	InsertNodeAtTail(&pHead, 2);
+	InsertNodeAtTail(&pHead, 3);
+	InsertNodeAtTail(&pHead, 4);
+
+	//DeleteNode(&pHead, 1);	// Head 삭제 테스트
+	TravelseNode(pHead);
+
+	//DeleteNode(&pHead, 2);		// 중간 삭제 테스트
+	TravelseNode(pHead);
+
+	DeleteNode(&pHead, 4);		// tail 삭제 테스트
+	TravelseNode(pHead);
+}
+
+#endif
